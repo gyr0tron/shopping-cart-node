b745fb1e7608efe2ce7ce8d45e112df3b704ac35
gyr0tron <simmarkalsi@gmail.com>
2018-03-15 05:30:59 +0530

    Added partials

--- app.js
+++ app.js
@@ -4,14 +4,19 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var expressHbs = require('express-handlebars');
+var mongoose = require('mongoose');
 
 var index = require('./routes/index');
 
 var app = express();
 
+//Mongoose setup
+mongoose.connect(process.env.DB_URL || 'mongodb://localhost/shoppingCart')
+
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
+app.set('view engine', '.hbs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
