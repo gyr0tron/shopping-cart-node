6f57029e80d64b2ce24f707efeb759ac7064a7f1
gyr0tron <simmarkalsi@gmail.com>
2018-03-19 22:36:27 +0530

    added cart add functionality

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var Cart = require('../models/cart');
 
 //load product model
 var Product = require('../models/product');
@@ -13,6 +14,22 @@ router.get('/', function(req, res, next) {
       productChunks.push(docs.slice(i, i+chunkSize))
     }
     res.render('shop/index', { title: 'Shopping', products: productChunks });
+  });
+});
+
+router.get('/add-to-cart/:id', function (req, res, next) {
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart ? req.session.cart : {items: {}});
+
+  Product.findById(productId, function(err, product) {
+    if (err) {
+      // error not yet handled
+      return res.redirect('/');
+    }
+    cart.add(product, product.id);
+    req.session.cart = cart;
+    console.log(req.session.cart);
+    res.redirect('/');
   });
 });
 
